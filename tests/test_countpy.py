c3568c80724c65143755fcfeb283ec6e2bb40a6b
dtdykstra <dtdykstra@scrumalliance.org>
2025-04-28 15:49:02 -0600

    test: add unit test for count_words

--- tests/test_countpy.py
+++ tests/test_countpy.py
@@ -1,1 +1,26 @@
-from countpy import countpy
+"""Test the countpy module."""
+
+# test_countpy.py
+from collections import Counter
+from countpy.countpy import count_words
+
+
+def test_count_words():
+    """Test word counting from a file."""
+    expected = Counter(
+        {
+            "insanity": 1,
+            "is": 1,
+            "doing": 1,
+            "the": 1,
+            "same": 1,
+            "thing": 1,
+            "over": 2,
+            "and": 2,
+            "expecting": 1,
+            "different": 1,
+            "results": 1,
+        }
+    )
+    actual = count_words("tests/einstein.txt")
+    assert actual == expected, "Einstein quote counted incorrectly!"
